78468bd99396d377278a3e8343f58d4c576b3581
BuildTools <unconfigured@null.spigotmc.org>
2020-05-23 08:41:30 -0500

    Final commit

---
+++ CS155/Assignment2/Assignment2.py
@@ -0,0 +1,16 @@
+#****************************************************************
+#
+#   CS155: Computer Science
+#   Fall Semester, 2017
+#   
+#   Assignment #1: Intro to Python
+#   Initalize variables and perform basic operations
+#
+#   Programmed by: Jacob Morris
+#
+#**************************************************************** 
+val1, val2 = 10, 30
+print(val1+val2)
+print(val1-val2)
+print(val1*val2)
+print(val1/val2)
